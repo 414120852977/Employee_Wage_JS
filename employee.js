82f5456f3b24320e8f79f8d2309ec60ec6fc0c3d
Ashok Mane <ashokmane81800@gmail.com>
2021-05-31 08:41:12 +0530

    [ashok] added to check employee is present or abscent

---
+++ employee.js
@@ -0,0 +1,5 @@
+// uc1 ability to check employee is present or abscent
+let empCheck = Math.floor(Math.random()*2)
+if (empCheck == 1) 
+    console.log("employee is present");
+else console.log("employee is abscent");
