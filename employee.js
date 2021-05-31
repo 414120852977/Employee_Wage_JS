60b122bc0f4db8bf1c7aca909e3157c623c8b7b4
Ashok Mane <ashokmane81800@gmail.com>
2021-05-31 12:42:03 +0530

    [ashok] added calculating wages per month

--- employee.js
+++ employee.js
@@ -3,6 +3,7 @@ const IS_PRESENT= 1;
 const PART_TIME_HOUR = 4;
 const FULL_TIME_HOUR = 8;
 const WAGE_PER_HOUR= 20;
+const MONTHLY_WORKING_DAYS = 2;
 let empCheck = Math.floor(Math.random()*3);
 
 function getWorkingHours(empCheck) {
@@ -21,3 +22,10 @@ function getWorkingHours(empCheck) {
 emphr = getWorkingHours(empCheck);
 let empWage = emphr * WAGE_PER_HOUR;
 console.log("employee wage:"+empWage);
+
+for(let i = 1; i <= MONTHLY_WORKING_DAYS; i++) {
+let empCheck = Math.floor(Math.random()*10)%3;
+emphr += getWorkingHours(empCheck);
+}
+let empWage1 = emphr * WAGE_PER_HOUR;
+console.log("total hours"+emphr +"employee wage"+empWage1);
