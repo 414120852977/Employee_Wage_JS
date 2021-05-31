adaf7ae58476fb87235d3623e90b2f02afa180f1
Ashok Mane <ashokmane81800@gmail.com>
2021-05-31 11:56:17 +0530

    [ashok] added daily employee wage of a employee

--- employee.js
+++ employee.js
@@ -1,6 +1,21 @@
 console.log("welcome to employee wage");
 const Is_Present = 1;
-let empCheck = Math.floor(Math.random()*2);
-if (empCheck == Is_Present) 
-    console.log("employee is present");
-else console.log("employee is abscent");
+let emphr=0 ;
+const wage_per_hr = 20;
+let empCheck = Math.floor(Math.random()*3);
+switch(empCheck) {
+    case 0:
+    console.log("employee is abscent");
+    emphr = 0;
+    break;
+    case 1:
+    console.log("employee is full time");
+    emphr = 8;
+    break;
+    case 2:
+    console.log("employee is part time");
+    emphr = 4;
+    break;
+}
+let dailyWage = wage_per_hr * emphr;
+console.log("total daily wage of a employee is:"+dailyWage)
